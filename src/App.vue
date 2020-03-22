40d3c36c0d4f1f51b2e49ee98b495e7824b49a1e
zigelzi <miika.a.savela@gmail.com>
2020-03-22 13:54:23 +0200

    Add axios sanity check to App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div>
       <h2>This is test from Docker with VUE</h2>
       <p>How's life inside container?</p>
+      {{ testData.message }}
     </div>
   </div>
 </template>
@@ -29,3 +30,31 @@
   }
 }
 </style>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      testData: {}
+    };
+  },
+  methods: {
+    getData() {
+      const path = "/sanity";
+      axios
+        .get(path)
+        .then(res => {
+          this.testData = res.data;
+        })
+        .catch(error => {
+          //eslint-disable-next-line
+        console.error(error);
+        });
+    }
+  },
+  mounted() {
+    this.getData();
+  }
+};
+</script>
